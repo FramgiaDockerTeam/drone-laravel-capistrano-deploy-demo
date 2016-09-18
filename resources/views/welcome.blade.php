778a10c7a6fcc5b4252dbf4d25fecced483905a2
Tran Duc Thang <thangtd90@gmail.com>
2016-09-18 17:37:17 +0700

    Update version 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             <h1 class="text-primary text-center">Auto deployed by <strong>Capistrano</strong>!</h1>
             <h2> You are in <strong>{{ app()->environment() }} </strong> environment!</h2>
             <h3 class="text-success text-center">Server Local IP: <strong>{{ isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : 'local' }}</strong></h3>
-            <h3 class="text-danger text-center">Version: <strong>1</strong></h3>
+            <h3 class="text-danger text-center">Version: <strong>2</strong></h3>
             <br>
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome to a Demo Site</div>
